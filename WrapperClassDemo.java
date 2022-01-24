a1f0ef860e30f20f2dd3ba7e44e1ffc2e5e634db
Excalibur79 <ankursahamoto@gmail.com>
2022-01-24 14:04:21 +0530

    wrapper class

---
+++ WrapperClassDemo.java
@@ -0,0 +1,10 @@
+/**
+ * WrapperClassDemo
+ */
+public class WrapperClassDemo {
+
+    public static void main(String[] args) {
+        Integer i = 6; // AutoBoxing or Auto wrapping
+        System.out.println(i.intValue());
+    }
+}
